371dd5515c648bbea8765b09badda82ef32c35ca
Lucas Miranda <lucasmirandaneves@gmail.com>
2024-06-23 00:05:35 -0300

    feat: Add AppDbContext and Initial Migration

---
+++ src/fcamara-test-dotnet.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+using fcamara_test_dotnet.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace fcamara_test_dotnet.Infrastructure.Data;
+
+public class AppDbContext: DbContext
+{
+    public DbSet<Establishment> Establishments { get; set; }
+	public DbSet<Vehicle> Vehicles { get; set; }
+	public DbSet<VehicleEntry> VehiclesEntry { get; set; }
+	public DbSet<VehicleExit> VehiclesExit { get; set; }
+
+	// public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+		optionsBuilder.UseSqlite("Data Source=./Data/database.db");
+
+        base.OnConfiguring(optionsBuilder);
+    }
+}
