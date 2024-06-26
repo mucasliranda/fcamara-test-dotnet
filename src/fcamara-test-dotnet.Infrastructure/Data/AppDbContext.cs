1e9d9b3b17b77044e3264bdb6cfa20c72987b708
Lucas Miranda <lucasmirandaneves@gmail.com>
2024-06-26 16:41:52 -0300

    fix: implements AppDbContextFactory for dev migration purpose

--- src/fcamara-test-dotnet.Infrastructure/Data/AppDbContext.cs
+++ src/fcamara-test-dotnet.Infrastructure/Data/AppDbContext.cs
@@ -10,12 +10,5 @@ public class AppDbContext: DbContext
 	public DbSet<VehicleEntry> VehiclesEntry { get; set; }
 	public DbSet<VehicleExit> VehiclesExit { get; set; }
 
-	public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }
-
-    // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    // {
-	// 	optionsBuilder.UseSqlite("Data Source=./Data/database.db");
-
-    //     base.OnConfiguring(optionsBuilder);
-    // }
+    public AppDbContext(DbContextOptions<AppDbContext> options): base(options) { }
 }
